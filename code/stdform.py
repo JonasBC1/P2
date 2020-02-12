09acf26488bfeb2b4ed0692abd5d4755ced80042
Mads B. Kjær <mbk1dk@gmail.com>
2020-02-12 16:11:31 +0100

    Update stdform.py

---
+++ code/stdform.py
@@ -0,0 +1,17 @@
+f	=	"4x1 + 6x2 + x3 = z"
+
+A	=[	
+"12x1 - x2 =< 15",
+"3x2 + 2x3 =< 10",
+"x1 => 0",
+"x2 => 0",
+"x3 => 0"]
+
+
+def stdform(f=f, A=A):
+	mx	=	f.count("x")
+	
+	for i in range(1, mx + 1):
+		
+
+stdform()
