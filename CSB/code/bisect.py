c6a2e8e1fb26e0bba5da474a87b9daa860e7d68a
Mads B. Kjær <mbk1dk@gmail.com>
2020-04-10 14:41:33 +0200

    Update bisect.py and newton-iter.py

--- CSB/code/bisect.py
+++ CSB/code/bisect.py
@@ -5,15 +5,7 @@
 import math
 
 def fun1(x):
-    return 1 - x + math.sin(x)
-    
-def fun2(x):
-    return 4*x**3 - x**2 -12*x + 3
-    
-def fun3(x):
-    return math.exp(x)-0.7
-
-    
+    return x*1/2*(math.exp(75/x) + math.exp(-75/x)) - x - 15
     
 def bisect(f, a, b, eps):
     if f(a) * f(b) > 0:
@@ -30,23 +22,11 @@ def bisect(f, a, b, eps):
                 a=m
         return (a + b)/2.0, nit
 
-#Opgave 4. Nulpunkt ved (0;-1;-0.357) 27 iterationer (-100;100;35 it) E-12 48 it
-x=-100
-y=100
-myeps=1E-12 
-res, nit = bisect(fun3,x,y,myeps)
+x=1
+y=200
+myeps=1E-12
 
-#Opgave 3 nulpunkt (1;2;1,732) 27 it, (-1;1;0,25) 28 it og (-2;-1;-1,732) 27 it
-#x=1
-#y=2
-#myeps=1E-8
-#res, nit = bisect(fun2,x,y,myeps)
-
-#Opgave 2 (1;2;1.934) 27 it
-#x=1
-#y=2
-#myeps=1E-8
-#res, nit    =   bisect(fun1,x,y,myeps)
+res, nit = bisect(fun1,x,y,myeps)
 
 tol = myeps/2.0
 
--- CSB/code/newton-iter.py
+++ CSB/code/newton-iter.py
@@ -14,19 +14,14 @@ def iter(f,df,xinit,tol):
 		xnew = xold - f(xold) / df(xold)
 	return xnew, k
 		
-	
 def fun1(x):
-	return x**2-10.0
+	return x*math.cosh(75/x) - x - 15
 	
 def dfun1(x):
-	return 2.0*x
+	return math.cosh(75/x)-75/x*math.sinh(75/x) - 1
 	
+sol, n = iter(fun1, dfun1, 100, 1E-9)
 
-	
-sol, n = iter(fun1, dfun1, 1, 1E-9)
-
-
-	
 print('Approximation is %8.7E' % sol)
 
 print('Number of interations used is %d' % n)
