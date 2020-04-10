ee8720bbfd130b56d58cfd34d78f50b592f3bf67
Julie Havbo Lund <jlund19@student.aau.dk>
2020-04-10 15:21:43 +0200

    Update sekant.py, sekant_noter.py, and 14d.tex

--- CSB/code/sekant.py
+++ CSB/code/sekant.py
@@ -1,8 +1,9 @@
 import numpy as np
+import math
 
-def secant(f,x0,x1,eps): 
+def secant(f,x0,x1,tol): 
     count = 0  
-    while abs(x0 - x1) > eps:  
+    while abs(x0 - x1) > tol:  
         x_temp = x1-(f(x1)*(x1-x0))/(f(x1)-f(x0)) 
         x0 = x1 
         x1 = x_temp 
@@ -10,22 +11,14 @@ def secant(f,x0,x1,eps):
     return x1, count  
 
 def fun_test(x):
-    return np.exp(x) - 5*x+2
+    return x*math.cosh(75/x) - x - 15
 
-x0 = 2
-x1 = 3
-eps = 1E-12
+x0 = 10
+x1 = 100
+tol = 1E-12
 
-s,count = secant(fun_test, x0, x1, eps)
+s,count = secant(fun_test, x0, x1, tol)
 
 print ('x0 = %g and x1 = %g' % (x0,x1))
 print ('A zero is located at: %19.7E' % s)
 print ('Number of interations used: %d' % count)
-
-import matplotlib.pyplot as plt 
-x = np.linspace(2,3,100,endpoint=True)
-plt.plot(x, fun_test(x))
-plt.grid(axis = 'both')
-plt.title('$e^x-5x+2$')
-plt.xlabel('x')
-plt.ylabel('y')
--- CSB/code/sekant_noter.py
+++ CSB/code/sekant_noter.py
@@ -1,9 +1,9 @@
 # Sekantmetoden 
 import numpy as np
 
-def secant(f,x0,x1,eps): # Funktionen 
+def secant(f,x0,x1,tol): # Funktionen 
     count = 0  # Tæller op 
-    while abs(x0 - x1) > eps: # Numerisk værdi 
+    while abs(x0 - x1) > tol: # Numerisk værdi 
         x_temp = x1-(f(x1)*(x1-x0))/(f(x1)-f(x0)) # Approksimerede løsning
         x0 = x1 # Sætter x0 til x1 
         x1 = x_temp # Sætter x1 til den nye værdi 
@@ -11,23 +11,15 @@ def secant(f,x0,x1,eps): # Funktionen
     return x1, count # Returnere 
 
 def fun_test(x):
-    return np.exp(x) - 5*x+2
+    return x*math.cosh(75/x) - x - 15
 
-x0 = 2
-x1 = 3
-eps = 1E-12
+x0 = 10
+x1 = 100
+tol = 1E-12
 
-s,count = secant(fun_test, x0, x1, eps)
+s,count = secant(fun_test, x0, x1, tol)
 
 print ('x0 = %g and x1 = %g' % (x0,x1))
 print ('A zero is located at: %19.7E' % s)
 print ('Number of interations used: %d' % count)
 
-# plot
-import matplotlib.pyplot as plt 
-x = np.linspace(2,3,100,endpoint=True)
-plt.plot(x, fun_test(x))
-plt.grid(axis = 'both')
-plt.title('$e^x-5x+2$')
-plt.xlabel('x')
-plt.ylabel('y')
