5317e5d791196a62af1687400106f92832ebb895
Jonas <Jonas@DESKTOP-36IQ0T7.local>
2020-04-10 09:52:10 +0200

    Update delopgave1_1.py

---
+++ CSB/code/delopgave1_1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 09:36:23 2020
+
+@author: Jonas
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+h0 = 10
+T = 0.5
+p = 0.5
+L = T/p
+l = 10
+def cosh(x):
+    return(0.5*(math.exp(x)+math.exp(-x)))
+   
+  
+x = np.array(range(int(-l/2),int((l/2)+1)))
+
+y = []
+for k in x:
+    y_h = h0 + L*cosh(k/L)
+    y.append(y_h)
+np.asarray(y)
+
+
+plt.plot(x,y)
+
+plt.show()
